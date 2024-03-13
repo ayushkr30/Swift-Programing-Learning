9c8a56e7a5bdf9feaba062b9b23f7c43cf2bfc39
Ayush Kumar <ayushpal2017ak@gmail.com>
2024-03-13 23:17:06 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Swift-Programing-Learning
  A hands-on practise of Swift Programming by Reading Documentation of Swift at Swift.org 
+
+
+
---
+++ TheBasic.swift
@@ -0,0 +1,21 @@
+//
+//  TheBasic .swift
+//  
+//
+//  Created by Ayush Kumar Pal on 13/03/24.
+//
+
+import Foundation
+
+/*Constants and Variable should be created before they're used */
+
+
+var numberOfDays = 10  //variables
+let month = 12         //constants
+
+var x= 0.0, y=0.0, z= 0.0
+//We can declare mutliple constants or multiple variables in a  single line,separated by commas.
+
+
+
+
